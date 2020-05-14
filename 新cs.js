b7416c18af7ba85dfc4fba0e4643943c604a2b2f
jin <894958343@qq.com>
2020-05-14 18:00:22 +0800

    '0'

--- 新cs.js
+++ 新cs.js
@@ -1,88 +1,105 @@
-旧
-RadioGroup  
-新
-RadioButtonGroup
+import { React, ReactDOM, openSdk, Button } from '@alife/icbu-mod-lib';
+import './index.scss';
 
-export default class Editor extends DesignEditor {
-  static info = {
-    type: 'extension-cnzoom-goodsswiper03-3',
-    name: '商品分组A3',
-    description: '个性轮播展示，可自动切换，效果突出醒目。',
-    icon: 'https://img.yzcdn.cn/public_files/2019/02/12/a6806f6ff8c220aa7a57eb89d253e126.png',
-    maxNum: 20,
-    usedNum: 0,
-    status: '',
-  };
+class IntlIcbuSmodDemo extends React.Component {
 
-  static getInitialValue() {
-    return {
-      type: 'extension-cnzoom-goodsswiper03-3',
-      tabs: [
-        { name: '商品分组一', goods: [] },
-        { name: '商品分组二', goods: [] },
-        { name: '商品分组三', goods: [] },
-      ],
-      title_style: '4',
-      col_num: '3',
-      goods_gap: 10,
-      goods_padding: 0,
-      goods_border_width: 0,
-      goods_border_color: '#eeeeee',
-      goods_background_color: '',
-      goods_radius: 0,
-      goods_pic_scale: '100',
-      goods_pic_fill: 'cover',
-      info_align: '2',
-      show_list: ['price'],
-
-      title_lines: '1',
-      title_font_size: '14',
-      title_font_weight: '700',
-      title_color: '#333333',
-      desc_font_size: '12',
-      desc_font_weight: '400',
-      desc_color: '#999999',
-      price_font_size: '18',
-      price_font_weight: '700',
-      price_xsd: '1',
-      price_qz: '￥',
-      price_color: '#333333',
-      origin_color: '#999999',
-      btn_icon: '1',
-      btn_text: '立即下单',
-      btn_color: '#ffffff',
-      btn_background_type: '3',
-      btn_linear_start: '#fd8831',
-      btn_linear_end: '#fc5008',
-      btn_radius: '15',
-      sale_font_weight: '400',
-      sale_color: '#666666',
-      sale_color2: '#000000',
-
-      padding_top: 10,
-      padding_bottom: 0,
-      padding_horiziontal: 5,
-      background_color: '',
-
-      main_padding_top: 0,
-      main_padding_bottom: 0,
-      main_padding_horiziontal: 0,
-      main_background_color: '',
-      main_radius_top: 0,
-      main_radius_bottom: 0,
-
-      title_width: 25,
-      title_height: 30,
-      title_margin: 5,
-      title_radius: 25,
-      title_border_width: 2,
-      title_border_color_def: '#eeeeee',
-      background_def: '#eeeeee',
-      color_def: '#333333',
-      title_border_color_act: '#333333',
-      background_act: '#333333',
-      color_act: '#333333',
-      title_size: '14',
-      title_weight: '400',
-    };
+  constructor(props) {
+    super(props);
+    this.state = {
+      hasData: false,
+      curr: '0'
+    }
   }
+
+  componentWillMount() {
+    const moduleData = this.props.moduleData;
+    const { mds, gdc } = moduleData;
+    //如果是从API上取数据
+    const fetch = openSdk.fetch('icbu.data.common.minisite', { bizId: gdc.bizId, productIds: [60738071807, 60736303506], strategyName: 'manuallySelect' });
+    fetch.then((data) => {
+    })
+  }
+  handleClick = (i) => {
+    this.setState({
+      curr: i
+    })
+  }
+  render() {
+    const { hasData, moduleTitle } = this.state;
+    const { mds } = this.props.moduleData;
+    var mk_tits = mds.moduleData.mk_tits ? Number.parseInt(mds.moduleData.mk_tits) : '';
+    var paddingTop = mds.moduleData.paddingTop ? Number.parseInt(mds.moduleData.paddingTop) : '';
+    var paddingBottom = mds.moduleData.paddingBottom ? Number.parseInt(mds.moduleData.paddingBottom) : '';
+    if (mds.moduleData.target === true) {
+      var target = "_blank";
+    } else {
+      var target = "_self";
+    }
+    /* 模块背景图片设置*/
+    var mk_bgDis = mds.moduleData.mk_bgDis == null || mds.moduleData.mk_bgDis == '1' ? '1' : '2';
+    var mk_bgXg = mds.moduleData.mk_bgXg == null || mds.moduleData.mk_bgXg == 'scroll' ? 'scroll' : 'fixed';
+
+    var demo = [{ "img": "https://img.alicdn.com/imgextra/i4/800803731/O1CN015Rd7Kz1dQqJrgYiAt_!!800803731.jpg" },
+    { "img": "https://img.alicdn.com/imgextra/i4/800803731/O1CN01DN1wb11dQqJzeBgi1_!!800803731.jpg" },
+    { "img": "https://img.alicdn.com/imgextra/i2/800803731/O1CN01W5RPEY1dQqK4Uzv00_!!800803731.jpg" }];
+    var item_info = mds.moduleData.item_info ? mds.moduleData.item_info : demo;
+    return (
+      <div className={'wm1920'} style={{ background: 'url("' + (mk_bgDis == '1' ? (mds.moduleData.mk_bgImg ? mds.moduleData.mk_bgImg : '') : '') + '") center top no-repeat ' + mk_bgXg, backgroundColor: mds.moduleData.mk_bg, paddingTop: paddingTop, paddingBottom: paddingBottom }}>
+        {mds.moduleData.btitle && <div className='tit_box'>
+          <div className='title' style={{ color: mds.moduleData.mk_titc, fontFamily: mds.moduleData.mk_titf, fontSize: mk_tits }}>{mds.moduleData.btitle}</div>
+          <div className='title_icon'><img src={mds.moduleData.title_icon ? mds.moduleData.title_icon : 'https://img.alicdn.com/imgextra/i4/800803731/O1CN01mTeoxD1dQqJHxQumU_!!800803731.png'} /></div>
+        </div>}
+
+        <div className="mk-bd slide">
+          <div className="panles">
+            <div className="pic trans03s" style={{background: "url('https://sc01.alicdn.com/kf/HTB1H9F4B3KTBuNkSne1q6yJoXXak.jpg') no-repeat center center"}}></div>
+            <div className="pic trans03s" style={{background: "url('https://sc01.alicdn.com/kf/HTB15Q.ZBIyYBuNkSnfoq6AWgVXa1.jpg') no-repeat center center"}}></div>
+            <div className="pic trans03s" style={{background: "url('https://sc01.alicdn.com/kf/HTB1ck_Yc6fguuRjSspkq6xchpXaX.jpg') no-repeat center center"}}></div>
+            <div className="pic trans03s" style={{background: "url('https://sc01.alicdn.com/kf/HTB1MmWxKh9YBuNjy0Ffq6xIsVXaI.jpg') no-repeat center center"}}></div>
+            <div className="pic trans03s show" style={{background: "url('https://sc01.alicdn.com/kf/HTB1zF81pLImBKNjSZFlq6A43FXau.jpg') no-repeat center center"}}></div>
+          </div>
+
+          <div className="panle">
+            <a target="_blank" className="item">
+              <div className="info">
+                <div className="line"></div>
+                <p className="title">Fashion Modern</p>
+                <div className="buy"><span className="zm">&gt;</span><span className="fm">&gt;</span></div>
+              </div>
+            </a>
+            <a target="_blank" className="item">
+              <div className="info">
+                <div className="line"></div>
+                <p className="title">Northern New</p>
+                <div className="buy"><span className="zm">&gt;</span><span className="fm">&gt;</span></div>
+              </div>
+            </a>
+            <a target="_blank" className="item">
+              <div className="info">
+                <div className="line"></div>
+                <p className="title">Simple Fashion</p>
+                <div className="buy"><span className="zm">&gt;</span><span className="fm">&gt;</span></div>
+              </div>
+            </a>
+            <a target="_blank" className="item">
+              <div className="info">
+                <div className="line"></div>
+                <p className="title">Extravagant</p>
+                <div className="buy"><span className="zm">&gt;</span><span className="fm">&gt;</span></div>
+              </div>
+            </a>
+            <a target="_blank" className="item hover">
+              <div className="info">
+                <div className="line"></div>
+                <p className="title">Korean Pastoral</p>
+                <div className="buy"><span className="zm">&gt;</span><span className="fm">&gt;</span></div>
+              </div>
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  }
+};
+
+export default IntlIcbuSmodDemo;
