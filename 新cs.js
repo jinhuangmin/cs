8dc4b3721485750dfb6a6529c0629d7725246aa5
jin <894958343@qq.com>
2020-07-16 14:28:24 +0800

    '.'

--- 新cs.js
+++ 新cs.js
@@ -21,7 +21,7 @@ class IntlIcbuSmodDemo extends React.Component {
     const { mds } = this.props.moduleData;
     var { url0, url1, url2, url3, url4, url5 } = mds.moduleData;
     var paddingTop = mds.moduleData.paddingTop ? Number.parseInt(mds.moduleData.paddingTop) : '';
-    var paddingBottom = mds.moduleData.paddingBottom ? Number.parsInt(mds.moduleData.paddingBottom) : '';
+    var paddingBottom = mds.moduleData.paddingBottom ? Number.parseInt(mds.moduleData.paddingBottom) : '';
     var mk_tits = mds.moduleData.mk_tits ? Number.parseInt(mds.moduleData.mk_tits) : '';
     var mk_bgDis = mds.moduleData.mk_bgDis == null || mds.moduleData.mk_bgDis == '1' ? 1 : '2';
     var mk_bgXg = mds.moduleData.mk_bgXg == null || mds.moduleData.mk_bgXg == 'scroll' ? 'scroll' : 'fixed';
@@ -30,7 +30,7 @@ class IntlIcbuSmodDemo extends React.Component {
     } else {
       var target = "_self";
     }
-    var mk_w = mds.moduleData.mk_w ? mds.moduleData.mk_w : 1440;
+    var mk_w = mds.moduleData.mk_w ? mds.moduleData.mk_w : 1440;  
 
     var imgInfo = ['https://img.alicdn.com/imgextra/i1/800803731/O1CN01PNUAOj1dQqGwJ9LLv_!!800803731.jpg',
       'https://img.alicdn.com/imgextra/i1/800803731/O1CN01CnwysF1dQqGwIsxz8_!!800803731.jpg',
@@ -42,7 +42,7 @@ class IntlIcbuSmodDemo extends React.Component {
 
     var imgitem = [];
     for (var i = 0; i < 6; i++) {
-      imgitem.push(<div className={'item item' + eval(i) + ' fl pic-style-' + mds.moduleData.animat}>
+      imgitem.push(<div className={'item item' + eval(i) + ' fl pic-style-'+mds.moduleData.animat}>
         <a target={target} href={eval('url' + i)}>
           <img className="trans03s" src={eval('mds.moduleData.img1440_' + i) ? eval('mds.moduleData.img1440_' + i) : imgInfo[i]} />
           <span></span>
