b2163c6a92825ad39750ba95f7990e42864462e7
jin <894958343@qq.com>
2020-11-13 10:46:41 +0800

    '更新了几个无线模版'

--- 新cs.js
+++ 新cs.js
@@ -1,168 +1,49 @@
-import { React, ReactDOM, openSdk, Button, Slider } from '@alife/icbu-mod-lib';
-import './index.scss';
-
-function wmCode(a) { if (/diyModule/.test(a)) { a = JSON.parse(a); var d = []; a = a.wm; for (var b in a) if (a[b]) { var c = a[b].t; var e = a[b].a; if (b < a.length - 2) c = React.createElement(c, e, null), d.push(c); else if (b == a.length - 2) return React.createElement(c, e, d) } } };
-
-class IntlIcbuSmodDemo extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      hasData: false,
-      result: [],
-      curr: '0',
-      auto: true,
-      close: false
-    }
-  }
-
-
-  componentWillMount() {
-    const moduleData = this.props.moduleData;
-    const { mds, gdc } = moduleData;
-    const fetch = openSdk.fetch('icbu.data.product.list', { bizId: gdc.bizId, strategyName: mds.strategyName, countNumber: mds.countNumber, products: mds.products });
-    fetch.then((data) => {
-      this.setState({
-        hasData: true,
-        result: data.success ? data.result : []
-      })
-    })
-  }
-
-  handleClick = (i) => {
-    this.setState({
-      curr: i
-    })
-  }
-  autoplay = (a) => {
-    this.setState({
-      auto: a
-    })
-  }
-
-  render() {
-    const { hasData, moduleTitle } = this.state;
-    const { mds, gdc } = this.props.moduleData;
-    const { auto } = this.state;
-    var banner_w = mds.moduleData.width ? Number.parseInt(mds.moduleData.width <= 20 ? 1920 : mds.moduleData.width) : 1920;
-    var banner_h = mds.moduleData.height ? Number.parseInt(mds.moduleData.height <= 20 ? 800 : mds.moduleData.height) : 800;
-    /*轮播图*/
-    var demo_img = 'https://img.alicdn.com/imgextra/i3/800803731/O1CN01ap5yUV1dQqLZlApsZ_!!800803731.jpg';
-    /*右侧悬浮产品图*/
-    var demoImg = ['https://img.alicdn.com/imgextra/i1/800803731/O1CN01W1wSch1dQqLV39L1O_!!800803731.jpg',
-      'https://img.alicdn.com/imgextra/i4/800803731/O1CN01x6cRsa1dQqLW8EaFl_!!800803731.jpg',
-      'https://img.alicdn.com/imgextra/i2/800803731/O1CN01rcgE4m1dQqLW8CuKQ_!!800803731.jpg',
-      'https://img.alicdn.com/imgextra/i4/800803731/O1CN01rtBXrQ1dQqLZlAhR5_!!800803731.jpg'];
-
-    var paddingTop = mds.moduleData.paddingTop ? Number.parseInt(mds.moduleData.paddingTop) : '';
-    var paddingBottom = mds.moduleData.paddingBottom ? Number.parseInt(mds.moduleData.paddingBottom) : '';
-    var xf_gdNum = Number.parseInt(mds.moduleData.xf_gdNum);
-    var xf_right = mds.moduleData.xf_right ? Number.parseInt(mds.moduleData.xf_right) : '';
-    var xf_top = mds.moduleData.xf_top ? Number.parseInt(mds.moduleData.xf_top) : '';
-    var dnum, speed;
-    banner_w > 1200 ? dnum = 16 : dnum = 10;
-    var imgw = banner_w / dnum;
-    var imgnum = Math.ceil(banner_h / imgw) * dnum;
-    var zbabylist = [];
-    var ml = (1920 - banner_w) / 2;
-    var random = mds.moduleData.animat;
-    mds.moduleData.animat == '1000' ? speed = 500 : speed = 0;
-
-    if (mds.moduleData.target === true) {
-      var target = "_blank";
-    } else {
-      var target = "_self";
-    }
-
-    var jtImgs = [{ "prev": "https://img.alicdn.com/imgextra/i1/800803731/O1CN01sm8LfF1dQqLuZy78w_!!800803731.png", "next": "https://img.alicdn.com/imgextra/i4/800803731/O1CN018jRfTg1dQqM221P7Y_!!800803731.png" },
-    { "prev": "https://img.alicdn.com/imgextra/i1/800803731/O1CN01xLrRdV1dQqLyBI3DT_!!800803731.png", "next": "https://img.alicdn.com/imgextra/i3/800803731/O1CN018NJfjF1dQqM18ygwN_!!800803731.png" },
-    { "prev": "https://img.alicdn.com/imgextra/i4/800803731/O1CN01uoYhmL1dQqLpv0UtF_!!800803731.png", "next": "https://img.alicdn.com/imgextra/i4/800803731/O1CN01BnCNTg1dQqLvhnwq0_!!800803731.png" },
-    { "prev": "https://img.alicdn.com/imgextra/i1/800803731/O1CN01TMxRZe1dQqLvhoouB_!!800803731.png", "next": "https://img.alicdn.com/imgextra/i1/800803731/O1CN01xwLjRH1dQqM0phYf0_!!800803731.png" },
-    { "prev": "https://img.alicdn.com/imgextra/i4/800803731/O1CN019BhXv51dQqM190yHO_!!800803731.png", "next": "https://img.alicdn.com/imgextra/i4/800803731/O1CN01e7mJH41dQqLxv4eZ9_!!800803731.png" }
-    ];
-
-    var jt_style = mds.moduleData.jt_style ? mds.moduleData.jt_style : '0';
-    var prevArrow = <div><img style={{ left: (banner_w / 2 - 600) }} src={mds.moduleData.jt_imgL ? mds.moduleData.jt_imgL : jtImgs[jt_style].prev} /></div>;
-    var nextArrow = <div><img style={{ right: (banner_w / 2 - 600) }} src={mds.moduleData.jt_imgR ? mds.moduleData.jt_imgR : jtImgs[jt_style].next} /></div>;
-    var banners = mds.moduleData.banners ? mds.moduleData.banners : [{ "image": demo_img, "add": "" }];
-    for (var j = 0; j < banners.length; j++) {
-      if (random == '100') { random = Number.parseInt(Math.random() * 11 + 1); }
-      var imgitem = [];
-      for (var i = 0; i < imgnum; i++) {
-        imgitem.push(<div className='imgbox' style={{ width: imgw, height: imgw }}><div style={{ backgroundImage: 'url(' + (banners[j].image ? banners[j].image : demo_img) + ')', backgroundPosition: '-' + imgw * (i % dnum) + 'px -' + imgw * (Number.parseInt(i / dnum)) + 'px' }} className={'img_item tx' + ((Number.parseInt(i / dnum) + i) % 2)}></div></div>);
-      }
-      zbabylist.push(<div className={j == this.state.curr ? 'product_box show' : 'product_box hide'}>
-        <a target={target} href={banners[j].add} className={'cf wm_animat animat_box' + (random)} onMouseEnter={this.autoplay.bind(null, false)} onMouseLeave={this.autoplay.bind(null, true)}>{imgitem}</a>
-      </div>);
-    }
-
-    var nav_style = mds.moduleData.nav_style;
-    var nav_width = [20, 30, 22, 22, 22];
-    var navlist = [];
-    if (nav_style < 2) {
-      var style_def = { background: mds.moduleData.nav_defc };
-      var style_act = { background: mds.moduleData.nav_actc };
-    } else if (nav_style == 2 || nav_style == 3) {
-      var style_def = { borderColor: mds.moduleData.nav_defc };
-      var style_act = { borderColor: mds.moduleData.nav_actc };
-    } else {
-      var style_def = { borderBottomColor: mds.moduleData.nav_defc };
-      var style_act = { borderBottomColor: mds.moduleData.nav_actc };
-    }
-
-    for (var i = 0; i < banners.length; i++) {
-      navlist.push(<li className={'list ' + (i == this.state.curr ? 'active' : '')} onMouseEnter={this.handleClick.bind(null, i)}>
-        <div className='def' style={style_def}></div>
-        <div className='act' style={style_act}></div>
-      </li>);
-    }
-
-
-
-    var itemList = [];
-    if (this.state.result.length != 0) {
-      {
-        this.state.result.map((item, index) => (
-          itemList.push(<a target={target} href={item.productUrl} className='list'>
-            <div className='img'><img src={item.productImage.url.x350} /></div>
-            <div className='price' style={{ color: mds.moduleData.xf_pricec }}>{item.fobPriceWithoutUnit}</div>
-          </a>
-          )
-        ))
-      }
-    } else {
-      for (var i = 0; i < 4; i++) {
-        itemList.push(<a className='list'>
-          <div className='img'><img src={demoImg[i]} /></div>
-          <div className='price'>US $90.0-168.0</div>
-        </a>
-        )
-      }
-    }
-
-    var xf_html = mds.moduleData.xf_html ? mds.moduleData.xf_html : '1';
-
-    return (<div className='wm1920 slider' style={{ backgroundColor: mds.moduleData.mk_bg }}>
-      <div className='mk_bd' style={{ height: banner_h, marginTop: paddingTop, marginBottom: paddingBottom }}>
-
-        {mds.moduleData.dots && banners.length > 1 && <ul className={'nav nay_type' + nav_style} style={{ left: (960 - nav_width[nav_style] * banners.length / 2) }}>{navlist}</ul>}
-
-        <Slider className='slider_box' style={{ width: banner_w, height: banner_h }} arrows={mds.moduleData.arrows ? true : false} speed={speed} autoplay={mds.moduleData.autoplay ? auto : false} autoplaySpeed={mds.moduleData.autoplaySpeed} nextArrow={nextArrow} prevArrow={prevArrow} dots={false} slickGoTo={this.state.curr} afterChange={(currentIndex) => this.setState({ curr: currentIndex })} >
-          {banners.map((item, index) => <a href={item.add} key={index} onMouseEnter={this.autoplay.bind(null, false)} onMouseLeave={this.autoplay.bind(null, true)} className="slider-img-wrapper" target={target} >{mds.moduleData.animat == '1000' && <img className='slider_img' src={item.image ? item.image : demo_img} />}</a>)}
-        </Slider>
-        {mds.moduleData.animat != '1000' && <div className='banner_box' style={{ width: banner_w, height: banner_h, marginLeft: ml }}>{zbabylist}</div>}
-      </div>
-
-      {mds.moduleData.xf_dis != '2' && <div className='hbxf' style={{ right: xf_right, top: xf_top }}>
-        <div className='close' onClick={() => this.setState({ close: !this.state.close })} style={{ borderColor: mds.moduleData.xf_bk }}>{this.state.close ? '+' : '×'}</div>
-        {mds.moduleData.xf_html && !this.state.close && wmCode(mds.moduleData.xf_html)}
-        {xf_html == "1" && <div className={this.state.close ? 'items hide' : 'items show'} style={{ backgroundColor: mds.moduleData.xf_bg, borderColor: mds.moduleData.xf_bk }}>
-          <Slider className='itembox' autoplay={true} autoplaySpeed={3000} dots={false} slideDirection={'vertical'} slidesToShow={3} arrows={false} style={{ height: (xf_gdNum * 180) }}>{itemList}</Slider>
-          <a target={target} href={mds.moduleData.xf_btnUrl}><img className='requests' src={mds.moduleData.xf_btn ? mds.moduleData.xf_btn : 'https://img.alicdn.com/imgextra/i4/800803731/TB24Yk0n67nBKNjSZLeXXbxCFXa_!!800803731.png'} /></a>
-        </div>}
-      </div>}
-    </div>
-    );
-  }
+const FILTER = {
+  // 1. industry 行业分类 multiple
+  industry: [
+    ["all", "全部行业"],
+    [1, "服装服饰"],
+    [2, "数码家电"],
+    [3, "建筑建材"],
+    [4, "机械配件"],
+    [5, "日用百货"],
+    [6, "运动户外"],
+    [7, "化妆美容"],
+    [8, "箱包鞋帽"],
+    [9, "汽摩汽配"],
+    [10, "玩具礼品"],
+    [11, "安防照明"],
+    [12, "家居园艺"],
+    [13, "五金工具"],
+    [14, "医药日化"],
+    [15, "包装印刷"],
+    [16, "食品茶饮"],
+    [17, "母婴用品"],
+    [18, "电气设备"],
+    [19, "办公文具"],
+    [20, "纺织皮具"],
+    [21, "宠物用品"],
+    [22, "原材料"],
+    [23, "能源化工"],
+    [24, "农产品"],
+    [25, "成人保健"],
+    [26, "矿物冶金"],
+  ],
+  // 3. color 色系分类 single
+  color: [
+    [1, "#0000ff"],//蓝色
+    [2, "#000000"],//黑色
+    [3, ["#000000", "#ffffff"]],//黑白
+    [4, "#00ff00"],//绿色
+    [5, "#ff0000"],//红色
+    [6, "#808080"],//灰色
+    [7, "#ffa500"],//橙色
+    [8, "#a67d3d"],//棕色
+    [9, "#ffff00"],//黄色
+    [10, "#ffc0cb"],//粉色
+    [11, "#800080"],//紫色
+    [12, "http://img.alicdn.com/tps/TB1dnT_PXXXXXcvaXXXXXXXXXXX-60-44.png"],//炫彩
+    [13, "#00ffff"],//青色
+    [14, "#ffd700"],//金色
+  ],
 };
-
-export default IntlIcbuSmodDemo;   
